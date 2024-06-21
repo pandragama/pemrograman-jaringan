30ef340b44b1fb660398a202b1bde2b4dba20029
Pandu Rafa Panatagama <123819826+pandragama@users.noreply.github.com>
2024-06-21 21:32:35 +0700

    Add files via upload

---
+++ Minggu 15 - 06 Juni 2024 - Praktikum/Broadcast/client1.py
@@ -0,0 +1,30 @@
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 8080
+s.connect((host, port))
+
+print("Menyambungkan ke Server")
+
+message = s.recv(1024)
+message = message.decode()
+print("Pesan dari server : ", message)
+
+while 1:
+    message = s.recv(1024)
+    message = message.decode()
+    print("Server : ", message)
+    new_message = input("Masukkan Pesan : ")
+    new_message = str(new_message).encode()
+    s.send(new_message)
+    print("Pesan Terkirim")
+    message = s.recv(1024)
+    message = message.decode()
+    print("Client_2 : ", message)
+    message = s.recv(1024)
+    message = message.decode()
+    print("Client_3 : ", message)
+    message = s.recv(1024)
+    message = message.decode()
+    print("Client_4 : ", message)
